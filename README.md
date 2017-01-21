dcef9aff5352955b415772b09399ed2066ccf7cd
takahiro-saeki <hiroooon69@gmail.com>
2017-01-21 18:21:57 +0900

    add move route.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 This is Style-it example.
 ##What is Style-it?
 Style-it is library for CSS with React.js.
-See [docs](https://github.com/buildbreakdo/style-it)
+See [docs](https://github.com/buildbreakdo/style-it)  
 twiter
 [@Sh_McK](https://twitter.com/Sh_McK)
 
--- src/js/container/App.jsx
+++ src/js/container/App.jsx
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import Base from '../Base/Base';
 import Header from '../component/Header';
+import IndexMain from '../component/IndexMain';
 
 const App = () => (
   <div>
     <Base />
     <Header />
+    <IndexMain />
   </div>
 )
 
